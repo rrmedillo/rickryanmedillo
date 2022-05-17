569892fb20d511cfa08384d0d4400b0119a92994
Rick Ryan <rickryan29.rr@gmail.com>
2022-05-17 20:23:40 +0800

    test04

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAC1b-XIKuTK9W12RTqynckEL_erWODNCU",
+    authDomain: "rickryanmedillo-7e31a.firebaseapp.com",
+    databaseURL: "https://rickryanmedillo-7e31a-default-rtdb.firebaseio.com",
+    projectId: "rickryanmedillo-7e31a",
+    storageBucket: "rickryanmedillo-7e31a.appspot.com",
+    messagingSenderId: "798061190719",
+    appId: "1:798061190719:web:258027ed517d17aae9628a",
+    measurementId: "G-FBGSR05GZD"
+  }
 };
